83d45650fc581c558f6bf582c2dde18aaee4772c
Alibobick <dozenkoalex6@gmail.com>
2026-05-25 21:47:00 +0300

    add solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -8,7 +8,7 @@ export const CommentList = ({ comments }) => {
         <div data-cy="NoCommentsMessage">No comments yet</div>
       )}
       {comments.map(comment => (
-        <CommentInfo comment={comment} />
+        <CommentInfo key={comment.id} comment={comment} />
       ))}
     </div>
   );
